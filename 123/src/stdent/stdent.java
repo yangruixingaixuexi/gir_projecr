426448e3bad017714b662c5cc6c40f697be65970
yangruixingaixuexi <501246364@qq.com>
2020-06-16 12:53:29 +0800

    提交到本地仓库

---
+++ 123/src/stdent/stdent.java
@@ -0,0 +1,17 @@
+package stdent;
+
+public class stdent {
+	private int id;
+	private int sex;
+	private String username;
+	private String password;
+	private String love;
+	public stdent(int id,String username,String password,int sex,String love){
+    super();
+    this.id=id;
+    this.sex=sex;
+    this.username=username;	
+    this.password=password;
+    this.love=love;
+}
+	}
